72ac6190195f0af2333e49002cb79235306cde90
Muhammad Shayan <iamshayan40@example.com>
2024-09-08 06:30:18 -0700

    latestt

--- app/blogpost/[slug]/page.js
+++ app/blogpost/[slug]/page.js
@@ -2,7 +2,7 @@ export default function Page({ params }) {
   const blog = {
     title: "Next.js",
     author: "John Doe",
-    description: "This is a sample blog post description. ",
+    description: "This is a sample blog post description.",
     date: "2024-09-02",
     content:
       "<p>This is the content of the blog post. It can include <strong>HTML</strong> tags and other elements.</p>",
@@ -12,9 +12,9 @@ export default function Page({ params }) {
 return(
   <div className="max-w-5xl mx-auto p-4">
     <h1 className="text-3xl font-bold mb-4 ">{blog.title}</h1>
-  <p className="text-lg mb-2">{blog.description}</p>
+  <p className="text-lg mb-2 border-l-4 border-gray-500 pl-4 italic">&quot;{blog.description}&quot;</p>
   <div className="flex gap-2">
-  <h1 className="text-sm text-gray-500 mb-4">{blog.author}</h1>
+  <h1 className="text-sm text-gray-500 mb- italic">By {blog.author}</h1>
   <p className="text-sm text-gray-500 mb-4">{blog.date}</p>
   </div>
   <div dangerouslySetInnerHTML={{ __html: blog.content }} className="prose"></div>
